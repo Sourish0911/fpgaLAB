3cdf9ee4cfbb6291261c5492ced63f99033aa30d
Sourish0911 <ee22mtech02002@iith.ac.in>
2022-02-06 14:25:36 +0530

    pio

--- Assignment 1/Full Adder Using NAND Gates.c
+++ Assignment 1/Full Adder Using NAND Gates.c
@@ -1,6 +1,8 @@
 // Online C compiler to run C program online
 #include<stdio.h>
 unsigned int nand(unsigned char A,const unsigned char B);
+unsigned int sum(unsigned char A,unsigned char B, unsigned char Cin);
+unsigned int carry(unsigned char A,unsigned char B, unsigned char Cin);
 
 int main()
 {
@@ -10,19 +12,8 @@ int main()
 
     //inputs for Half adder
    unsigned char A = 0x01, B = 0x01, Cin = 0x01;
-    
-
-    //Full ADDER Logic
-    unsigned char nand1 = nand(A,B);
-    unsigned char nand2 = nand(nand1,A);
-    unsigned char nand3 = nand(nand1,B);
-    unsigned char nand4 = nand(nand2,nand3);
-    unsigned char nand5 = nand(nand4,Cin);
-    unsigned char nand6 = nand(nand4,nand5);
-    unsigned char nand7 = nand(nand5,Cin);
-    S_FA = nand(nand6,nand7); 
-    Cout_FA = nand(nand5,nand1);
-    
+    S_FA = sum(0x01,0x01,0x01);
+    Cout_FA = carry(0x01,0x01,0x01);
     printf("Sum = %x Carry = %x",(unsigned int)S_FA, (unsigned int)Cout_FA);
     return 0;
 }
@@ -31,4 +22,31 @@ int main()
 unsigned int nand(unsigned char A,unsigned char B)
 {
    return !(A&B);
+}
+
+unsigned int sum(unsigned char A,unsigned char B, unsigned char Cin)
+{
+    unsigned char S_FA;
+    unsigned char nand1 = nand(A,B);
+    unsigned char nand2 = nand(nand1,A);
+    unsigned char nand3 = nand(nand1,B);
+    unsigned char nand4 = nand(nand2,nand3);
+    unsigned char nand5 = nand(nand4,Cin);
+    unsigned char nand6 = nand(nand4,nand5);
+    unsigned char nand7 = nand(nand5,Cin);
+    S_FA = nand(nand6,nand7); 
+    return S_FA;
+}
+
+unsigned int carry(unsigned char A,unsigned char B, unsigned char Cin)
+{
+    unsigned char nand1 = nand(A,B);
+    unsigned char nand2 = nand(nand1,A);
+    unsigned char nand3 = nand(nand1,B);
+    unsigned char nand4 = nand(nand2,nand3);
+    unsigned char nand5 = nand(nand4,Cin);
+    unsigned char nand6 = nand(nand4,nand5);
+    unsigned char nand7 = nand(nand5,Cin);
+    unsigned char Cout_FA = nand(nand5,nand1);
+    return Cout_FA;
 }
